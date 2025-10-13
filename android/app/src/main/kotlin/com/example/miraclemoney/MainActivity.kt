8a257aeb8e6e6dc37344b05e22674c9efa4f8d84
MiracleMoney <voyahoo12396@gmail.com>
2025-10-13 22:47:32 +0900

    첫번째" "

---
+++ android/app/src/main/kotlin/com/example/miraclemoney/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.miraclemoney
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/constants/gaps.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:miraclemoney/constants/sizes.dart';
+
+class Gaps {
+  // Vertical Gaps
+  static const v1 = SizedBox(height: Sizes.size1);
+  static const v2 = SizedBox(height: Sizes.size2);
+  static const v3 = SizedBox(height: Sizes.size3);
+  static const v4 = SizedBox(height: Sizes.size4);
+  static const v5 = SizedBox(height: Sizes.size5);
+  static const v6 = SizedBox(height: Sizes.size6);
+  static const v7 = SizedBox(height: Sizes.size7);
+  static const v8 = SizedBox(height: Sizes.size8);
+  static const v9 = SizedBox(height: Sizes.size9);
+  static const v10 = SizedBox(height: Sizes.size10);
+  static const v11 = SizedBox(height: Sizes.size11);
+  static const v12 = SizedBox(height: Sizes.size12);
+  static const v14 = SizedBox(height: Sizes.size14);
+  static const v16 = SizedBox(height: Sizes.size16);
+  static const v20 = SizedBox(height: Sizes.size20);
+  static const v24 = SizedBox(height: Sizes.size24);
+  static const v28 = SizedBox(height: Sizes.size28);
+  static const v32 = SizedBox(height: Sizes.size32);
+  static const v36 = SizedBox(height: Sizes.size36);
+  static const v40 = SizedBox(height: Sizes.size40);
+  static const v44 = SizedBox(height: Sizes.size44);
+  static const v48 = SizedBox(height: Sizes.size48);
+  static const v52 = SizedBox(height: Sizes.size52);
+  static const v56 = SizedBox(height: Sizes.size56);
+  static const v60 = SizedBox(height: Sizes.size60);
+  static const v64 = SizedBox(height: Sizes.size64);
+  static const v72 = SizedBox(height: Sizes.size72);
+  static const v80 = SizedBox(height: Sizes.size80);
+  static const v96 = SizedBox(height: Sizes.size96);
+}
