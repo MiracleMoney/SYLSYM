4a632899f3c32972984fb3ca600f68fd689f89b4
MiracleMoney <voyahoo12396@gmail.com>
2026-03-01 15:42:17 +0900

    예산 항목 리스트 아이콘 색상 최적화

--- lib/features/budget/presentation/widgets/budget_item_card.dart
+++ lib/features/budget/presentation/widgets/budget_item_card.dart
@@ -39,7 +39,8 @@ class BudgetItemCard extends StatelessWidget {
               width: 36,
               height: 36,
               decoration: BoxDecoration(
-                color: categoryColor.withOpacity(0.1),
+                color: Colors.white,
+                border: Border.all(color: categoryColor, width: 0.5),
                 borderRadius: BorderRadius.circular(10),
               ),
               child: Icon(iconData, color: categoryColor, size: 18),
